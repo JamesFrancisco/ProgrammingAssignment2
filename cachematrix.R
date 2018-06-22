14c187d3081f9ecedbd33b90ad7c3172ffceede9
James Francisco <james.r.francisco@outlook.com>
2018-06-21 21:18:15 -0700

    Version of the assignment ready to turn in.
    Signed-off-by: James Francisco <james.r.francisco@outlook.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create, invert, cache, and then write out a matrix 
+## They represent the work product for the second assignment for R Programming
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates and inverts the matrix
 makeCacheMatrix <- function(x = matrix()) {
+## Initialize the variable i and the y function
+	inverted <- NULL
+	set <- function(y) {
+	      ## The <<- operator assigns a value to an object in a different environment
+          x <<- y
+          inverted <<- NULL
+	}	
+ ## Define and compute the inverted matrix
+	get = function() x
+	setinv <- function(inverse) inverted <<- inverse
+	getinv <- function() inverted
+	list(set=set,
+		 get=get,
+		 setinv = setinv,
+		 getinv = getinv)
+ 
+}	 
 
-}
 
-
-## Write a short comment describing this function
+## cacheSolve returns the inverted matrix calculated by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invert <- x$getinv()
+  if(!is.null(invert)){
+    message("Loading the cached matrix")
+    return(invert)
+  }
+ ## Structuring and writing the inverted matrix 
+  data <- x$get()
+  invert <- solve(data)
+  x$setinv(invert)
+  invert     
 }
